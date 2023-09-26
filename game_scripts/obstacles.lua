d59b79da5bcdc7ff2a082263e0a480d96dffb579
Max Weber <weber.max.1988@gmail.com>
2023-09-26 08:21:21 +0200

    Update

--- game_scripts/obstacles.lua
+++ game_scripts/obstacles.lua
@@ -1,25 +1,16 @@
 -- this function needs to be named exactly "doObstacles" to be called by host Dart env
 function doObstacles()
   -- mkObstacle is defined by the host Dart env
-  mkObstacle(350, 365, visible());
-  mkObstacle(600, 465, visible());
-  mkObstacle(200, 150, visible());
-  mkObstacle(600, 70, visible());
-  mkObstacle(700, 190, visible());
+  mkObstacle(10, -1);
+  mkObstacle(15, 7);
+  mkObstacle(2, 7);
+  mkObstacle(-16, 2);
 end
 
--- returns bool, true for visible
-function visible()
-  return randomBool(0.6)
+function doWeather()
+  mkWeather(0)
 end
 
--- limit is number 0 to 1 excluding 1 above which 
--- the random number needs to be generated to return true
-function randomBool(minimum)
-  local r = random()
-  --print(r)
-  if r > minimum
-    then return true
-  end
-  return false
+function doDayNight()
+  mkDayNight(1);
 end
