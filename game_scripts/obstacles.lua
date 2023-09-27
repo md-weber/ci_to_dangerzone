0311f869403390fba5eae09d6ab273efc3797232
Max Weber <weber.max.1988@gmail.com>
2023-09-27 10:09:03 +0200

    Update lua

--- game_scripts/obstacles.lua
+++ game_scripts/obstacles.lua
@@ -1,16 +1,16 @@
 -- this function needs to be named exactly "doObstacles" to be called by host Dart env
 function doObstacles()
   -- mkObstacle is defined by the host Dart env
-  -- mkObstacle(10, -1);
-  -- mkObstacle(15, 7);
+  mkObstacle(10, -1);
+  mkObstacle(15, 7);
   mkObstacle(2, 7);
   mkObstacle(-16, 2);
 end
 
 function doWeather()
-  mkWeather(0)
+  mkWeather(2)
 end
 
 function doDayNight()
-  mkDayNight(0);
+  mkDayNight(1);
 end
